3e8aed6f83646991021a5a3502e70fe05545ec18
Pablo Cantero <pablo@pablocantero.com>
2012-11-15 09:25:47 -0200

    skips user authentication on show

--- app/controllers/pro/test_mails_controller.rb
+++ app/controllers/pro/test_mails_controller.rb
@@ -1,11 +1,11 @@
 class Pro::TestMailsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, only: [:index]
   def index
     @mails = current_user.test_mails
   end
 
   def show
-    @mail = current_user.test_mails.find_by_token params[:id]
+    @mail = TestMail.find_by_token params[:id]
     render "show", layout: false
   end
 end
