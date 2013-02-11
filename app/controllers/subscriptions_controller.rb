49a315bc1b8efe8af67ae9c42a93bbf9e1865723
Pablo Cantero <pablo@pablocantero.com>
2013-02-11 16:09:34 -0200

    Uses yaml for configuration

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -33,43 +33,33 @@ class SubscriptionsController < ApplicationController
 end
 
 require "paypal/recurring"
-class InvalidRecurringPayment < Exception; end
+class PaypalSubscriptionException < Exception; end
 class PaypalSubscription
   attr_accessor :token, :payer_id, :reference
 
   def initialize token=nil, payer_id=nil, reference=nil
-    PayPal::Recurring.configure do |config|
-      config.sandbox   = true
-      config.username  = "seller_1354580504_biz_api1.pablocantero.com"
-      config.password  = "1354580523"
-      config.signature = "AiPC9BjkCyDFQXbSkoZcgqH3hpacAHqtmvCOKJGgALmZG7.b5SzXsIQF"
-    end
-    @token       = token
-    @payer_id    = payer_id
-    @reference   = reference
-    @description = "Puts Mail Pro Account Subscription"
-    @amount      = "2.99"
-    @currency    = "USD"
-    @ipn_url     = "http://localhost:3000/pro/paypal/ipn"
+    @token     = token
+    @payer_id  = payer_id
+    @reference = reference
   end
 
   def create_recurring_profile
     ppr = PayPal::Recurring.new({
-      description:  @description,
-      amount:       @amount,
-      currency:     @currency,
-      ipn_url:      @ipn_url,
-      frequency:    1,
       token:        @token,
-      period:       :monthly,
       reference:    @reference,
       payer_id:     @payer_id,
+      frequency:    1,
+      period:       :monthly,
       start_at:     Time.now,
       failed:       1,
-      outstanding:  :next_billing
+      outstanding:  :next_billing,
+      description:  CONFIG["paypal"]["description"],
+      amount:       CONFIG["paypal"]["amount"],
+      currency:     CONFIG["paypal"]["currenty"],
+      ipn_url:      CONFIG["paypal"]["ipn_url"]
     })
     response = ppr.create_recurring_profile
-    raise InvalidRecurringPayment unless response.valid?
+    raise PaypalSubscriptionException unless response.valid?
     OpenStruct.new profile_id: response.profile_id
   end
 
@@ -77,26 +67,26 @@ class PaypalSubscription
     ppr = PayPal::Recurring.new({
       token:        @token,
       payer_id:     @payer_id,
-      description:  @description,
-      amount:       @amount,
-      currency:     @currency
+      description:  CONFIG["paypal"]["description"],
+      amount:       CONFIG["paypal"]["amount"],
+      currency:     CONFIG["paypal"]["currenty"]
     })
     response = ppr.request_payment
-    raise InvalidRecurringPayment unless response.valid?
+    raise PaypalSubscriptionException unless response.valid?
     OpenStruct.new approved?: response.approved? && response.completed?
   end
 
   def checkout
     ppr = PayPal::Recurring.new({
-      return_url:   "http://localhost:3000/pro/paypal/thank_you",
-      cancel_url:   "http://localhost:3000/pro/paypal/canceled",
-      ipn_url:      @ipn_url,
-      description:  "Puts Mail Pro Account Subscription",
-      amount:       "2.99",
-      currency:     "USD"
+      return_url:   CONFIG["paypal"]["return_url"],
+      cancel_url:   CONFIG["paypal"]["cancel_url"],
+      ipn_url:      CONFIG["paypal"]["ipn_url"],
+      description:  CONFIG["paypal"]["description"],
+      amount:       CONFIG["paypal"]["amount"],
+      currency:     CONFIG["paypal"]["currenty"]
     })
     response = ppr.checkout
-    raise InvalidRecurringPayment unless response.valid?
+    raise PaypalSubscriptionException unless response.valid?
     OpenStruct.new url: response.checkout_url
   end
 end
---
+++ config/initializers/paypal.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+PayPal::Recurring.configure do |config|
+  config.sandbox   = CONFIG["paypal"]["sandbox"]
+  config.username  = CONFIG["paypal"]["username"]
+  config.password  = CONFIG["paypal"]["password"]
+  config.signature = CONFIG["paypal"]["signature"]
+end
